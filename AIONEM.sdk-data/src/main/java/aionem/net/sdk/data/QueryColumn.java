b22b82a9f2157b19407e6dcc41970e5045535d48
Alain NIYONEMA <alain.niyonema>
2023-12-12 00:15:36 +0200

    AIONEM.NET Web - ver1.9.0

--- AIONEM.sdk-data/src/main/java/aionem/net/sdk/data/QueryColumn.java
+++ AIONEM.sdk-data/src/main/java/aionem/net/sdk/data/QueryColumn.java
@@ -20,27 +20,31 @@ public class QueryColumn {
     }
     protected QueryColumn(final String column, final Object value) {
         this.column = column;
-        if(value instanceof QueryFunction) {
-            this.value = value.toString();
-        }else {
-            this.value = "'"+ UtilsText.toString(value) +"'";
-        }
+        this.value = getValue(value);
     }
     protected QueryColumn(final String column, final String logic, final Object value) {
         this.column = column;
-        this.value = " "+ logic +" " + "'"+ UtilsText.toString(value) +"'";
+        this.value = " "+ logic +" " + getValue(value);
     }
     protected QueryColumn(final String table, final String column, final String logic, final Object value) {
         this.column = table + "." + "`" + column + "`";
-        this.value = " "+ logic +" " + "'"+ UtilsText.toString(value) +"'";
+        this.value = " "+ logic +" " + getValue(value);
     }
     protected QueryColumn(final String operator, final String table, final String column, final String logic, final Object value) {
         this.column = " "+ operator +" " + table + "." + "`" + column + "`";
-        this.value = " "+ logic +" " + "'"+ UtilsText.toString(value) +"'";
+        this.value = " "+ logic +" " + getValue(value);
     }
     protected QueryColumn(final String column, final String logic, final String column1, final String value) {
         this.column = column;
         this.value = " "+ logic +" " + "`"+ column1 +"`" +" "+ UtilsText.toString(value);
+    }
+
+    public String getValue(final Object value) {
+        if(value instanceof QueryFunction) {
+            return value.toString();
+        }else {
+            return "'"+ UtilsText.toString(value) +"'";
+        }
     }
 
     @Override
